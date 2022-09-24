03687949c161550bc04ec2a6167cefa8c128de57
gregclar2019 <daddygreggy09@gmail.com>
2022-09-24 13:44:58 -0400

    tictactoe files

---
+++ tic-tac-toe/test.py
@@ -0,0 +1,108 @@
+test = ["x", "x", "x", "d", "e", "f", "g", "h", "i"]
+
+newlist = [x for x in test if type(x) == int]
+
+print(newlist)
+
+
+
+
+
+
+
+
+
+"""
+
+test = ["x", "x", "x", "d", "e", "f", "g", "h", "i"]
+
+newlist = [x for x in test if type(x) != int]
+
+if newlist == []:
+    print('TRUE')
+
+else:
+    print('FALSE')
+
+
+test = ["a", "b", "c", "o", "o", "o", "g", "h", "i"]
+
+x1 = f'{test[0]}{test[1]}{test[2]}'
+x2 = f'{test[3]}{test[4]}{test[5]}'
+x3 = f'{test[6]}{test[7]}{test[8]}'
+x4 = f'{test[0]}{test[3]}{test[6]}'
+x5 = f'{test[1]}{test[4]}{test[7]}'
+x6 = f'{test[2]}{test[5]}{test[8]}'
+x7 = f'{test[0]}{test[4]}{test[8]}'
+x8 = f'{test[2]}{test[4]}{test[6]}'
+
+o1 = f'{test[0]}{test[1]}{test[2]}'
+o2 = f'{test[3]}{test[4]}{test[5]}'
+o3 = f'{test[6]}{test[7]}{test[8]}'
+o4 = f'{test[0]}{test[3]}{test[6]}'
+o5 = f'{test[1]}{test[4]}{test[7]}'
+o6 = f'{test[2]}{test[5]}{test[8]}'
+o7 = f'{test[0]}{test[4]}{test[8]}'
+o8 = f'{test[2]}{test[4]}{test[6]}'
+
+if x1 == "xxx" or x2 == "xxx" or x3 == "xxx" or x4 == "xxx" or x5 == "xxx" or x6 == "xxx" or x7 == "xxx" or x8 == "xxx":
+    print('Winner X')
+
+if o1 == "ooo" or o2 == "ooo" or o3 == "ooo" or o4 == "ooo" or o5 == "ooo" or o6 == "ooo" or o7 == "ooo" or o8 == "ooo":
+    print('Winner o')
+
+
+#test = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+test = "abcdefghi"
+test2 = []
+for x in range(0,3):
+    test2.append("|".join(test[3*x:(3*x + 3)]))
+
+print(f"\n-+-+-\n".join(te
+
+test = "abcdefghi"
+test2 = []
+for x in range(0,3):
+    "|".join(test2.append(test[3*x:(3*x + 3)]))
+
+print(f"\n-+-+-\n".join(test2))
+
+from cgitb import text
+from genericpath import samefile
+
+
+test = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+i = 0
+
+test2 = []
+for x in range(0, 9, 3):
+    sample = ""
+    for y in range(x, int((3*x + 3))):
+        sample += f'{y}'
+    print(sample)
+
+
+
+
+
+from cgitb import text
+
+
+
+test = [1,2,3,4,5,6,7,8,9]
+i = 0
+
+test2 = []
+while i < len(test)/3:
+    j = 0
+    sample = ""
+    while 3*i < j < (3*i + 3):
+        sample += f'{test[j]}'
+        j += 1
+    test2.append(sample)
+    i += 1
+
+
+print(test2)
+
+"""
---
+++ tic-tac-toe/tic-tac-toe.py
@@ -0,0 +1,144 @@
+
+from pickle import TRUE
+
+
+grid_array = []
+
+def create_grid():
+    for x in range(1, 10):
+        grid_array.append(x)
+
+def print_grid():
+    array2txt = ""
+    grid2display= []
+    print()
+    for y in range(0, 9):
+        array2txt += f"{grid_array[y]}"
+
+    for i in range(0, 3):
+        grid2display.append("|".join(array2txt[3*i:(3*i + 3)]))
+    
+    print(f"\n-+-+-\n".join(grid2display))
+
+def is_it_draw():
+    #create a new list and save the remining integers of grid_array
+    newlist = [x for x in grid_array if type(x) == int]
+
+    #if we can't find any more integers, this means 
+    # all squares have already been taken by either x or o
+    #otherwise there's still available square to be marked by either x or o
+    if newlist == []:
+        return True
+    else:
+        return False
+
+def get_player_input(player):
+        #make sure that the user provides a number between 1 and 9 and that the chosen number is no longer taken
+        while True:
+            print()
+            user_input = int(input(f'{player}\'s turn to choose a square (1-9): '))
+
+            if user_input > 9:
+                print()
+                print('Please provide a number between 1 and 9.')
+            elif grid_array[user_input - 1] == "x" or grid_array[user_input - 1] == "o":
+                    print()
+                    print('This square is already taken')
+            else:
+                return user_input - 1
+
+def check_winner():
+
+    x1 = f'{grid_array[0]}{grid_array[1]}{grid_array[2]}'
+    x2 = f'{grid_array[3]}{grid_array[4]}{grid_array[5]}'
+    x3 = f'{grid_array[6]}{grid_array[7]}{grid_array[8]}'
+    x4 = f'{grid_array[0]}{grid_array[3]}{grid_array[6]}'
+    x5 = f'{grid_array[1]}{grid_array[4]}{grid_array[7]}'
+    x6 = f'{grid_array[2]}{grid_array[5]}{grid_array[8]}'
+    x7 = f'{grid_array[0]}{grid_array[4]}{grid_array[8]}'
+    x8 = f'{grid_array[2]}{grid_array[4]}{grid_array[6]}'
+
+    o1 = f'{grid_array[0]}{grid_array[1]}{grid_array[2]}'
+    o2 = f'{grid_array[3]}{grid_array[4]}{grid_array[5]}'
+    o3 = f'{grid_array[6]}{grid_array[7]}{grid_array[8]}'
+    o4 = f'{grid_array[0]}{grid_array[3]}{grid_array[6]}'
+    o5 = f'{grid_array[1]}{grid_array[4]}{grid_array[7]}'
+    o6 = f'{grid_array[2]}{grid_array[5]}{grid_array[8]}'
+    o7 = f'{grid_array[0]}{grid_array[4]}{grid_array[8]}'
+    o8 = f'{grid_array[2]}{grid_array[4]}{grid_array[6]}'
+
+    if x1 == "xxx" or x2 == "xxx" or x3 == "xxx" or x4 == "xxx" or x5 == "xxx" or x6 == "xxx" or x7 == "xxx" or x8 == "xxx":
+        return 1
+
+    if o1 == "ooo" or o2 == "ooo" or o3 == "ooo" or o4 == "ooo" or o5 == "ooo" or o6 == "ooo" or o7 == "ooo" or o8 == "ooo":
+        return 2
+
+def run_game():
+    #initialize the current player
+    current_player = "x"
+    while is_it_draw() == False:
+
+        #getting the players's input and recording in in grid_array
+        given_index = get_player_input(current_player)
+        grid_array[given_index] = current_player
+
+        #players taking turns
+        if current_player == "x":
+            current_player = "o"
+        else:
+            current_player = "x"
+        
+        print_grid()
+
+        #check for winner
+        if check_winner() == 1:
+            print()
+            print('The winner is X')
+            break
+        if check_winner() == 2:
+            print()
+            print('The winner is O')
+            break
+    
+    if is_it_draw() == True:
+        print()
+        print("It is a draw")
+
+
+
+def main():
+    create_grid()
+    print_grid()
+    run_game()
+    print("Good game. Thanks for playing!")
+    
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+
+
+word = ["|".join("123"), "|".join("456"), "|".join("789")]
+def print_grid():
+    row_format = []
+    line_breaker = f"\n-+-+-\n" 
+    print(line_breaker.join(word))
+
+word = [1,2,3,]
+def print_grid():
+    sample = ""
+    for x in grid_array:
+        sample =  sample + f'{x}'       
+    print("|".join(sample))
+
+
+"""
